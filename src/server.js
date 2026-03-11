b3a30b9b275eea179a1930e174ed21fdb7a16dab
Akram Mulani <akramamulani786@gmail.com>
2026-03-11 17:10:52 +0530

    Frontend new origin added

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const app = express();
 /* ✅ CORS CONFIG */
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: ["http://localhost:5173", "https://vespera-web-app.vercel.app"], // frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
